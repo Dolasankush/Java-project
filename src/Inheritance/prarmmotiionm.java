8b60f84d946949eac6e92222c874c80fdb8f20c3
Durvesh Pisalkar <durveshwpisalkar@gmail.com>
2022-01-21 12:39:14 +0530

    pamotion program changed

--- src/Inheritance/prarmmotiionm.java
+++ src/Inheritance/prarmmotiionm.java
@@ -3,13 +3,15 @@ package Inheritance;
 public class prarmmotiionm {
 	
 	
-final static void m1(int p) {
+final static void m1(double d) {
 	
-	System.out.println(" "+p);
+	System.out.println(" "+d);
 }
 
 public static void main(String[] args) {
 	
 	m1('A');
+	m1(12);
+	m1(1.23);
 }
 }
